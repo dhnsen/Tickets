303518cd3c41bd5b4f88d2e80c0359d169c463f6
Dan <spag1024@hotmail.com>
2019-01-22 14:20:12 -0600

    created ticketsController and added create method. created viewmodel for tickets and a create view

---
+++ ServiceManagement/Controllers/TicketsController.cs
@@ -0,0 +1,29 @@
+﻿using ServiceManagement.Models;
+using ServiceManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiceManagement.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public TicketsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Tickets
+        public ActionResult Create()
+        {
+            var viewModel = new TicketFormViewModel
+            {
+                Customers = _context.Customers.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ ServiceManagement/ViewModels/TicketFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ServiceManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceManagement.ViewModels
+{
+    public class TicketFormViewModel
+    {
+        
+        public string Summary { get; set; }
+
+        
+        public string Description { get; set; }
+
+        public int Customer { get; set; }
+
+        public IEnumerable<Customer> Customers { get; set; }
+    }
+}
